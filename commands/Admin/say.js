58c899937ba93d0c3326ef3fd1a445e26026b2e0
Natoune <samdavaure@icloud.com>
2021-11-13 19:44:10 +0100

    Update say.js

--- commands/Admin/say.js
+++ commands/Admin/say.js
@@ -3,6 +3,7 @@ const { MESSAGES } = require('../../utils/constants');
 module.exports.run = (client, message, args, settings) => {
     const argMessage = message.content.replace(settings.prefix+"say ", "").replace(settings.prefix+"repeat ", "").replace(settings.prefix+"rep ", "");
     message.channel.send(argMessage);
+    message.delete();
 }
 
 module.exports.help = MESSAGES.COMMANDS.ADMIN.SAY;
